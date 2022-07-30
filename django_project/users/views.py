2a9f57bda04dad8268fb7ef30e6d75fbc3c48098
DevIvo81 <izpy81@gmail.com>
2022-07-30 21:53:11 +0200

    Admin page, user registration, crispy forms

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account successfully created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegistrationForm()
     return render(request, 
                 'users/register.html',
                 {'form' : form})
     
+
+
+
 
 def login(request):
     pass
